acc40c72e4be74b3df5e0a256d25b82ee82d02be
gcanasherrera <guadalupe.canasherrera@esa.int>
2026-02-09 22:06:03 +0000

    Deploying to gh-pages from @ gcanasherrera/gcanasherrera.github.io@827f68ee1055a519bd8147f2d634283200083d25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,33 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-international-day-of-women-and-girls-in-science",
         
